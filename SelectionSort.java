b10629f879ab65211bb7b98cd98024e63055fcd1
James Blankenship <jbla484@wgu.edu>
2022-06-09 17:19:07 -0700

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -6,13 +6,10 @@ public class SelectionSort {
         TIME COMPLEXITY - 0(n^2)
         */
 
-    public void sort() {
-
-        // declare int array to sort
-        int[] nums = {55, 17, 43, -12, 75, 13, 7};
+    public void sort(int[] input) {
 
         // outer loop to find the last sorted index and decreases as elements get sorted
-        for (int lastSortedIndex = nums.length - 1; lastSortedIndex > 0; lastSortedIndex--) {
+        for (int lastSortedIndex = input.length - 1; lastSortedIndex > 0; lastSortedIndex--) {
 
             // declare variable to hold largest found in the iteration
             int largest = 0;
@@ -21,7 +18,7 @@ public class SelectionSort {
             for (int i = 1; i < lastSortedIndex; ++i) {
 
                 // if the element is greater than the largest, set largest to i
-                if (nums[i] > nums[largest]) {
+                if (input[i] > input[largest]) {
 
                     // set largest variable
                     largest = i;
@@ -29,17 +26,12 @@ public class SelectionSort {
             }
 
             // function call to swap the two elements
-            swap(nums, largest, lastSortedIndex);
-        }
-
-        // print values of sorted array to console
-        for (int num : nums) {
-            System.out.println(num);
+            swap(input, largest, lastSortedIndex);
         }
     }
 
     // function to swap two numbers in the array
-    public static void swap(int[] array, int i, int j) {
+    public static void swap(int[] input, int i, int j) {
 
         // if elements are equal, return
         if (i == j) {
@@ -47,8 +39,8 @@ public class SelectionSort {
         }
 
         // swap elements
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        int temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
     }
 }
