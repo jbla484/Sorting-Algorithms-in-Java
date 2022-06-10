15f78cace2e002c67e6b91ad29abf9a971a841b7
James Blankenship <jbla484@wgu.edu>
2022-06-09 17:17:12 -0700

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,34 +6,26 @@ public class BubbleSort {
         TIME COMPLEXITY - O(n^2)
     */
 
-    public void sort() {
-        
-        // declare int array to sort
-        int[] nums = {55, 17, 43, -12, 75, 13, 7};
+    public void sort(int[] input) {
 
         // outer loop to find the last sorted index and decrease it as elements get sorted
-        for (int lastSortedIndex = nums.length - 1; lastSortedIndex > 0; lastSortedIndex--) {
+        for (int lastSortedIndex = input.length - 1; lastSortedIndex > 0; lastSortedIndex--) {
 
             // inner loop to iterate through the array up to the last sorted index
             for (int i = 0; i < lastSortedIndex; ++i) {
 
                 // if the element is greater than the next, swap the two elements
-                if (nums[i] > nums[i + 1]) {
+                if (input[i] > input[i + 1]) {
 
                     // call function to swap elements
-                    swap(nums, i, i + 1);
+                    swap(input, i, i + 1);
                 }
             }
-        }
-
-        // print values of sorted array to console
-        for (int num : nums) {
-            System.out.println(num);
         }
     }
 
     // function to swap two numbers in the array
-    public static void swap(int[] array, int i, int j) {
+    public static void swap(int[] input, int i, int j) {
 
         // if elements are equal, return
         if (i == j) {
@@ -41,8 +33,8 @@ public class BubbleSort {
         }
 
         // swap elements
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        int temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
     }
 }
