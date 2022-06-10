c45114d8fc69a2e22d7d1761bdafdfa2c335cd66
James Blankenship <jbla484@wgu.edu>
2022-06-09 17:21:34 -0700

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -6,32 +6,24 @@ public class InsertionSort {
         TIME COMPLEXITY - O(n^2)
     */
 
-    public void sort() {
-
-        // declare int array to sort
-        int[] nums = {55, 17, 43, -12, 75, 13, 7};
+    public void sort(int[] input) {
 
         // iterate through unsorted partition, adding elements to the sorted partition every iteration
-        for (int firstUnsortedIndex = 1; firstUnsortedIndex < nums.length; firstUnsortedIndex++) {
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < input.length; firstUnsortedIndex++) {
 
             // element to add to sorted partition, set to array element at firstUnsortedIndex
-            int newElement = nums[firstUnsortedIndex];
+            int newElement = input[firstUnsortedIndex];
 
             // declare i to use after loop
             int i;
 
             // iterate through sorted partition to find where to add new element
-            for (i = firstUnsortedIndex; i > 0 && nums[i - 1] > newElement; i--) {
-                nums[i] = nums[i - 1];
+            for (i = firstUnsortedIndex; i > 0 && input[i - 1] > newElement; i--) {
+                input[i] = input[i - 1];
             }
 
             // set array element to new element
-            nums[i] = newElement;
-        }
-
-        // print values of sorted array to console
-        for (int num : nums) {
-            System.out.println(num);
+            input[i] = newElement;
         }
     }
 }
